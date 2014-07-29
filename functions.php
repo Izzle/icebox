fc7e6689ea1818468841fe857819ac67757c6bff
Ron Martin <ronmartin89@gmail.com>
2014-07-29 10:43:45 -0700

    Added function icebox_post_meta to display author, date, category, tags, and comments.  Also always for the author to Edit the post if they are logged in.

--- functions.php
+++ functions.php
@@ -84,5 +84,60 @@ if (! function_exists( 'icebox_setup ') ) {
 }
 
 
+/**
+ * -----------------------------------------------------------------------------------------------
+ * 5.0 Display meta information for a specific post
+ * -----------------------------------------------------------------------------------------------
+ */
+if ( ! function_exists( 'icebox_post_meta' ) ) {
+	function icebox_post_meta() {
+		echo '<ul class="list-inline entry-meta">';
+
+		if ( get_post_type() === 'post' ) {
+			// If the post is sticky, mark it
+			if ( is_sticky() ) {
+				echo '<li class="meta-featured-post"><i class="fa fa-thumb-tack"></i> ' . __( 'Sticky', 'icebox' ) . '</li>';
+			}
+
+			// Get post author
+			printf(
+				'<li class="meta-author"><a href="%1$s" rel="author">%2$s</a></li>',
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+				get_the_author()
+			);
+
+			// Get the date
+			echo '<li class="meta-date"> ' . get_the_date() . ' </li>';
+
+			// The categories
+			$category_list = get_the_category_list( ', ' );
+			if ( $category_list ) {
+				echo '<li class="meta-categories"> ' . $category_list . ' </li>';
+			}
+
+			// The tags
+			$tag_list = get_the_tag_list( '', ', ' );
+			if ( $tag_list ) {
+				echo '<li class="meta-tags"> ' . $tag_list . ' </li>';
+			}
+
+			// Comments link
+			if ( comments_open() ) :
+				echo '<li>';
+				echo '<span class="meta-reply">';
+				comments_popup_link( __( 'Leave a comment', 'icebox' ), __( 'One comment so far', 'icebox' ), __( 'View all % comments', 'icebox') );
+				echo '</span';
+				echo '</li>';
+			endif;
+
+			// Edit link
+			if ( is_user_logged_in() ) {
+				echo '<li>';
+				edit_post_link( __( 'Edit', 'icebox' ), '<span class="meta-edit">', '</span>');
+				echo '</li>';
+			}
+		}
+	}
+}
 
   ?>
