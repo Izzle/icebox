a95c06b4973f3fe31285fd119dccf69168b14bc5
Ron Martin <ronmartin89@gmail.com>
2014-07-29 10:41:44 -0700

    Added content.php

---
+++ content.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+ * content.php
+ *
+ * The default template for displaying content
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<!-- Article Header -->
+	<header class="entry-header"> <?php 
+		// If the post has a thumbnail and its not password protected
+		// then display the thumbnail
+		if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+			<figure class="entry-thumbnail"><?php the_post_thumbnail(); ?></figure>
+		<?php endif; 
+
+		// If single page, display the title
+		// else, display the title in a link
+		if ( is_single() ) : ?>
+			<h1><?php the_title(); ?></h1>
+		<?php else : ?>
+			<h1><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php endif; ?>
+		
+		<p class="entry-meta">
+			<?php 
+				// Display the meta information
+				icebox_post_meta();
+			 ?>
+		</p>
+	</header><!-- end entry-header -->
+</article>
