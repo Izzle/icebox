b6d56c908f4399d186358f05d4d4659999316500
Ron Martin <ronmartin89@gmail.com>
2014-07-29 11:01:17 -0700

    Added article content page and footer.  Footer now includes author name and description

--- content.php
+++ content.php
@@ -31,4 +31,29 @@
 			 ?>
 		</p>
 	</header><!-- end entry-header -->
+
+	<!-- Article Content -->
+	<div class="entry-content">
+		<?php
+		if (is_search() ) {
+			the_excerpt();
+		} else {
+			the_content( __( 'Continue reading &rarr;', 'icebox' ) );
+
+			wp_link_pages();
+		}
+		?>
+	</div><!-- end entry-content-->
+
+	<!-- Article Footer -->
+	<footer class="entry-footer">
+		<?php 
+			// If we have a single page and the author bio exists
+			// then display it
+			if ( is_single() && get_the_author_meta( 'description' ) ) {
+				echo '<h2>' . __( 'Written by ', 'icebox' ) . get_the_author() . '</h2>';
+				echo '<p>' . the_author_meta( 'description' ) . '</p>';
+			}
+		 ?>
+	</footer><!--end entry-footer-->
 </article>
