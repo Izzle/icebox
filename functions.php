7963af73f47db40a00bcda56f3b6c411e009be30
Ron Martin <ronmartin89@gmail.com>
2014-07-31 11:49:03 -0700

    Updated index to include header

--- functions.php
+++ functions.php
@@ -80,7 +80,7 @@ if (! function_exists( 'icebox_setup ') ) {
 		);
 	}
 
-	add_action( 'after_theme_setup', 'icebox_setup' );
+	add_action( 'after_setup_theme', 'icebox_setup' );
 }
 
 
@@ -139,5 +139,29 @@ if ( ! function_exists( 'icebox_post_meta' ) ) {
 		}
 	}
 }
-
-  ?>
+/**
+ * -----------------------------------------------------------------------------------------------
+ * 6.0 Display navigation to the next/previous set of posts.
+ * -----------------------------------------------------------------------------------------------
+ */
+if (! function_exists( 'icebox_paging_nav' ) ) {
+	function icebox_paging_nav() { ?>
+		<ul>
+			<?php 
+				if ( get_previous_posts_link() ) : ?>
+				<li class="next">
+					<?php previous_posts_link( __( 'Newer Posts &rarr;', 'icebox' ) ) ?>
+				</li>
+				<?php endif;
+			 ?>
+			 <?php 
+				if ( get_next_posts_link() ) : ?>
+				<li class="previous">
+					<?php next_posts_link( __( '&larr; Older Posts', 'icebox' ) ) ?>
+				</li>
+				<?php endif;
+			 ?>
+		</ul> <?php
+	}
+}
+?>
